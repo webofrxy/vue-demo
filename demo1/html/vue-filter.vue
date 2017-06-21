17a5f8dd2232bda8b0b19ba501e287793bf0862f
webofrxy <913686609@qq.com>
2017-06-21 23:28:51 +0800

    vue-filter

---
+++ demo1/html/vue-filter.vue
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>vue-filter</title>
+</head>
+<script src="../js/vue.js"></script>
+<body>
+	<div id="app">
+		<input type="text" placeholder="vue-filter" v-model= "message">
+		<h3>{{ message | num }}</h3>
+	</div>
+</body>
+<script>
+	Vue.filter('num',function(value){
+		if(!value)return '';
+		value = value.toString();
+		return value.charAt(0).toUpperCase()+value.slice(1)
+	})
+	var vm = new Vue({
+		el: "#app",
+		data() {
+			return {
+				message:''
+			}
+		},
+		methods:function(){
+
+		}
+	})
+</script>
+</html>
