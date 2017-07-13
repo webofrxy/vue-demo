809598e72274b0249a6abdd4fa64653fe43bb352
renxy@hzanchu.com <renxy@hzanchu.com>
2017-07-13 13:53:49 +0800

    比较版本号 和 闭包总结

---
+++ demo1/skill/compareVersion.js
@@ -0,0 +1,18 @@
+
+/**
+比较版本号
+*/
+$.compareVersion = function(a, b) {
+    var as = a.split('.');
+    var bs = b.split('.');
+    if (a === b) return 0;
+
+    for (var i = 0; i < as.length; i++) {
+      var x = parseInt(as[i]);
+      if (!bs[i]) return 1;
+      var y = parseInt(bs[i]);
+      if (x < y) return -1;
+      if (x > y) return 1;
+    }
+    return 1;
+  };
