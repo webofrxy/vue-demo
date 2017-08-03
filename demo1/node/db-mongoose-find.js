fbfeac81e0c009e1aa2ac9773744c39ac797ec61
renxy@hzanchu.com <renxy@hzanchu.com>
2017-08-03 15:50:12 +0800

    mongoose

--- demo1/node/db-mongoose-find.js
+++ demo1/node/db-mongoose-find.js
@@ -46,16 +46,4 @@ myAccount.save(function(err){
 //     }
 // });
 
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test');
-
-// var Cat = mongoose.model('Cat', { name: String });
-
-// var kitty = new Cat({ name: 'Zildjian' });
-// kitty.save(function (err) {
-// if (err) {
-// 	console.log(err)
-// }
-// console.log('meow');
-// });
 
